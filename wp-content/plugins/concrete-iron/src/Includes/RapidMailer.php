a8ebc48e99c4c71092dd49b5cf5da800b934a0b1
milliephanillie <me@philiparudy.com>
2023-10-30 14:15:16 -0400

    add template

--- wp-content/plugins/concrete-iron/src/Includes/RapidMailer.php
+++ wp-content/plugins/concrete-iron/src/Includes/RapidMailer.php
@@ -28,18 +28,57 @@ class RapidMailer {
      * @return mixed
      */
     public function add_to_email( $notification, $form, $entry ) {
-
         error_log(print_r('An email has been sent. The notification is: ', true));
         error_log(print_r($notification['name'], true));
 
+        $data = [
+            'full_name' => '',
+            'email_address' => '',
+            'phone' => '',
+            'message' => '',
+            'listing_url' => '',
+            'name' => ''
+        ];
+
         // Check if the notification name matches
         if ( $notification['name'] == 'Contact Seller' ) {
+            $template_path = plugin_dir_path( CONCRETEIRON ) . 'templates/lead.php';
+
             foreach ($form['fields'] as $field) {
+                $field_id = (string)$field['id'];
                 if (isset($field['inputName']) && $field['inputName'] == 'ci_post_id') {
-                    $field_id = (string)$field['id'];
                     if (isset($entry[$field_id])) {
                         $post_id = $entry[$field_id];
-                        break;
+                    }
+                }
+
+                if (isset($field['inputName']) && $field['inputName'] == 'full_name') {
+                    if (isset($entry[$field_id])) {
+                        $data['full_name'] = $entry[$field_id];
+                    }
+                }
+
+                if (isset($field['inputName']) && $field['inputName'] == 'email_address') {
+                    if (isset($entry[$field_id])) {
+                        $data['email_address'] = $entry[$field_id];
+                    }
+                }
+
+                if (isset($field['inputName']) && $field['inputName'] == 'phone') {
+                    if (isset($entry[$field_id])) {
+                        $data['phone'] = $entry[$field_id];
+                    }
+                }
+
+                if (isset($field['inputName']) && $field['inputName'] == 'message') {
+                    if (isset($entry[$field_id])) {
+                        $data['message'] = $entry[$field_id];
+                    }
+                }
+
+                if (isset($field['inputName']) && $field['inputName'] == 'listing_url') {
+                    if (isset($entry[$field_id])) {
+                        $data['listing_url'] = $entry[$field_id];
                     }
                 }
             }
@@ -48,13 +87,19 @@ class RapidMailer {
                 return $notification; // Return if post_id is not found
             }
 
-            $seller_id = get_post_meta( $post_id, '_product-agent', true );
+            $seller_id      = get_post_meta( $post_id, '_product-agent', true );
+            $seller_info    = get_userdata($seller_id);
+            $seller_email   = $seller_info->user_email;
 
-            $business_owner = get_post_meta($post_id, '_business-owner', true);
-            $profile_email = get_post_meta($business_owner, '_profile-email', true);
-
-            $seller_info = get_userdata($seller_id);
-            $seller_email = $seller_info->user_email;
+            $business_owner = get_post_meta($post_id, '_product-business', true);
+            error_log(print_r('_product-business', true));
+            error_log(print_r($business_owner, true));
+            $business  = get_post($business_owner);
+            $business_author = get_userdata($business->post_author);
+            if($business_author) {
+                $data['name'] = $business_author->user_login;
+            }
+            $profile_email  = get_post_meta($business_owner, '_profile-email', true);
 
             error_log(print_r('The seller email is: ', true));
             error_log(print_r($seller_email, true));
@@ -72,8 +117,36 @@ class RapidMailer {
                 }
                 $notification['to'] .= $profile_email;
             }
+
+
+
+            $content = $this->get_template_content($template_path, $data);
+            error_log(print_r("the content of the template", true));
+            error_log(print_r($content, true));
+            error_log(print_r("the data", true));
+            error_log(print_r($data, true));
+            if($content) {
+                $notification['message'] = $content;
+            }
         }
 
         return $notification;
+    }
+
+    function get_template_content($template_path, $vars = array()) {
+        if(file_exists($template_path)) {
+            // Extract the variables to a local namespace
+            extract($vars);
+
+            // Start output buffering
+            ob_start();
+
+            // Include the template file
+            include $template_path;
+
+            // End buffering and return its contents
+            return ob_get_clean();
+        }
+        return false;
     }
 }
