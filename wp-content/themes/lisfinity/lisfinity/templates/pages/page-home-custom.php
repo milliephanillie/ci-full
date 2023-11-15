d299bb2f8a4bfa79937f4ccfc2a2dafdb3906644
milliephanillie <me@philiparudy.com>
2023-11-15 10:09:14 -0500

    adding the homepage custom template

--- wp-content/themes/lisfinity/lisfinity/templates/pages/page-home-custom.php
+++ wp-content/themes/lisfinity/lisfinity/templates/pages/page-home-custom.php
@@ -70,6 +70,7 @@
                     <!-- Banner | Home -->
                     <div class="banner--home">
                         <?php echo wp_kses_post( $banner_text ); ?>
+                        <p style="font-size: 24px; line-height: 32px; font-weight: bold; text-align: center;">Basic Listings (Normally $30) Free for a Limited Time!</p>
                     </div>
                 <?php endif; ?>
 
