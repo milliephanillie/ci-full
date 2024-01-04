a52b4e2bb1fcec174fea61ac10a9053d701ad08b
milliephanillie <me@philiparudy.com>
2024-01-04 13:40:53 -0500

    remove dubgging

--- wp-config.php
+++ wp-config.php
@@ -106,7 +106,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG', false );
 //define( 'WP_DEBUG_DISPLAY', true );
 define( 'WP_DEBUG_LOG', true );
 
